ee98709b513b2f7570003d7e7c252bd762c25df6
Paul Solt <PaulSolt@gmail.com>
2019-10-14 11:58:09 -0400

    Starter project

---
+++ Contacts-Swift-Objc/Contacts-Swift-Objc/ContactsTableViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ContactsTableViewController.swift
+//  Contacts-Swift-Objc
+//
+//  Created by Paul Solt on 10/14/19.
+//  Copyright © 2019 Lambda, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ContactsTableViewController: UITableViewController {
+	
+	// MARK: - Table view data source
+	
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		#warning("implement method")
+		return 0
+	}
+	
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		
+		#warning("implement method")
+		
+		return UITableViewCell()
+	}
+}
